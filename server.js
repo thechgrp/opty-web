6130fce1c26f499b7f882f9fae0a1ac3062c3735
Nathaniel Alba <nathaniel.alba@gmail.com>
2018-10-18 12:05:06 -0700

    files

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const handler = require('serve-handler')
 const http = require('http')
+const serveOptions = require('./lib/options')
 
 const PORT = process.env.PORT || 3000
 
-const server = http.createServer((req, res) => handler(req, res))
+const server = http.createServer((req, res) => handler(req, res, serveOptions))
 
 server.listen(PORT, () => {
   console.log(`Accepting connections @ :${PORT}`)
